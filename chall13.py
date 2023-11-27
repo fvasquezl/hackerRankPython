92c3321a6abbccf39bc68e91229d05dcc9295330
Faustino Vasquez <vasquez.limon@gmail.com>
2023-11-26 18:35:21 -0800

    13

--- chall13.py
+++ chall13.py
@@ -1,14 +1,21 @@
+import heapq
 
-from operator import itemgetter
+if __name__ == "__main__":
+    arr = [
+        ("Rachel", -50),
+        ("Mawer", -50),
+        ("Sheen", -50),
+        ("Shaheen", 51),
+    ]
+    # for _ in range(int(input())):
+    #     name = input()
+    #     score = float(input())
+    #     arr.append((name, score))
+    lowest_elements = set(arr)
+    print(lowest_elements)
+    second_score = sorted(lowest_elements)[1][-1]
 
-if __name__ == '__main__':
-    arr=[]
-    for _ in range(int(input())):
-        name = input()
-        score = float(input())
-        arr.append([name,score])
-        
-    sorted_list = sorted(arr, key=itemgetter(1))
+    el = sorted([list(i) for i in arr if i[1] == second_score])
 
-    print(sorted_list)
- 
+    for i in el:
+        print(i[0])
