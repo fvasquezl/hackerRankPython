cf885fb4041dc5fab85d50267e7aab0454afec63
Faustino Vasquez <vasquez.limon@gmail.com>
2023-11-28 15:20:40 -0800

    15

--- chall15.py
+++ chall15.py
@@ -1,8 +1,29 @@
 if __name__ == "__main__":
     arr = []
+    out =[]
     N = int(input())
-    for i in range(N):
-        el = int(input().strip())
+    for _ in range(N):
+        el = input().split()
         arr.append(el)
 
-    print(arr)
+    for action in arr:
+        match action[0]:
+            case "insert":
+                op1= int(action[1])
+                op2= int(action[2])
+                out.insert(op1,op2)
+            case  'remove':
+                op1= int(action[1])
+                out.remove(op1)
+            case 'append':
+                op1= int(action[1])
+                out.append(op1)
+            case 'sort':
+                out.sort()
+            case 'pop':
+                out.pop()
+            case 'reverse':
+                out.reverse()
+            case 'print':
+                print(out)
+                
