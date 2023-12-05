a9bdbfafcb93581f6b58f409e9c5314fa8ce053b
Faustino Vasquez <vasquez.limon@gmail.com>
2023-12-05 15:57:56 -0800

    22-25

--- chall22.py
+++ chall22.py
@@ -1,21 +1,20 @@
 if __name__ == "__main__":
     s = input()
-    a, b, c, d, e : bool
+    op = [0,0,0,0,0]
+
     for i in s:
-        if i.isalnum() and a == True:
-            print(i.isalnum())
-            a = False
-        elif i.isalpha() and b:
-            print(i.isalpha())
-            b = False
-        elif i.isdigit() and c:
-            print(i.isdigit())
-            c = False
-        elif i.islower() and d:
-            print(i.islower())
-            d = False
-        elif i.isupper() and e:
-            print(i.isupper())
-            e = False
-        elif (a, b, c, d == False):
+        if i.isalnum() and op[0] == 0 :
+            op[0]=1
+        if i.isalpha() and op[1] == 0:
+            op[1]=1
+        if i.isdigit() and op[2] == 0:
+            op[2]=1
+        if i.islower() and op[3] == 0:
+            op[3]=1
+        if i.isupper() and op[4] == 0:
+            op[4]=1
+        if (op==[1,1,1,1,1]):
             break
+        
+    for i in op:
+        print(bool(i))
---
+++ chall25.py
@@ -0,0 +1,6 @@
+N,M = map(int, input().split())
+z=".|."
+for i in range(N):
+ 
+    print((z).rjust(5,"-"))
+    
