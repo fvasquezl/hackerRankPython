09bfa09b8d3074425757678eb94074148bfc9ef6
Faustino Vasquez <vasquez.limon@gmail.com>
2023-12-06 12:08:48 -0800

    25and 26

--- chall25.py
+++ chall25.py
@@ -1,7 +1,15 @@
 N,M = map(int, input().split())
 z=".|."
-for i in range(N):
- 
-    print((z).rjust(5,"-"))
-    
-    
+j = 1
+i=1
+while i < N + 1:
+    if i == (N + 1) / 2:
+        print(("WELCOME").center(M, "-"))
+        j -= 2
+    else:
+        print((z * j).center(M, "-"))
+        if i < (N + 1) / 2:
+            j += 2
+        elif i > (N + 1) / 2:
+            j -= 2
+    i += 1
