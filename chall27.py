2d89e19b18b20da329d9d60a7f6b7b03a82b7913
Faustino Vasquez <vasquez.limon@gmail.com>
2023-12-06 23:41:35 -0800

    27ok

--- chall27.py
+++ chall27.py
@@ -1,26 +1,29 @@
-import string
+import textwrap
 
 
 def print_rangoli(size):
-
-    L = list(map(chr, range(97, size+97)))
-    M = len(L)*2-1
-    j=0
-    k=-1
-
-    for _ in range(M):
-        r = L[j:]
-        e = list(reversed(r))[:k]
-        print(e,r)
-        j+=1
-        k-=1
-
-   
-    # for _ in range(list(map(chr, range(97, 123)))):
-    #     print("text","-")
-    
+    L = list(map(chr, range(97, size + 97)))
+    lt = (len(L) * 2 - 1) * 2 - 1
+    l1 = []
+    l2 = []
+    fl = fr = ""
+    if lt > 1:
+        for i in L[::-1]:
+            l1.append(fl + i + fr)
+            l2.insert(0, fl + i + fr)
+            fr = i + fr
+            fl = fl + i
+        print((l1[0]).center(lt, "-"))
+        for i in l1[1:]:
+            print("-".join(i).center(lt, "-"))
+        for i in l2[1:-1]:
+            print("-".join(i).center(lt, "-"))
+        print((l2[-1]).center(lt, "-"))
+    else:
+        print(L[0])
     return
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     n = int(input())
     print_rangoli(n)
