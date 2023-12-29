85e283f0fe9c98aed862f339fcab1ea8eef7c055
Faustino Vasquez <vasquez.limon@gmail.com>
2023-12-29 15:45:04 -0800

    62

--- 60 to 69/chall62.py
+++ 60 to 69/chall62.py
@@ -2,13 +2,10 @@ import math
 
 if __name__ == "__main__":
     AB = int(input())
-    a = int(input())
-    H = math.sqrt(AB**2 + a**2)
-    b = H / 2
-    C = math.degrees(math.atan(AB / a))
-    c = math.sqrt((a**2 + b**2) - ((2 * a * b) * math.cos(C)))
-    B = round(math.degrees(math.acos((a**2 + b**2 - c**2) / (2 * a * b))))
+    BC = int(input())
+    print(f"{round(math.degrees(math.atan(AB / BC)))}\u00b0")
+
+    # c = math.sqrt((a**2 + b**2) - ((2 * a * b) * math.cos(C)))
+    # B = round(math.degrees(math.acos((a**2 + b**2 - c**2) / (2 * a * b))))
 
     # A = math.degrees(math.acos((b**2 + c**2 - a**2) / (2 * b * c)))
-
-    print(B)
