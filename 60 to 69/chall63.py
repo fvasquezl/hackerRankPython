d8306bec2861ea7e5d77927dd06c434a9fca8915
Faustino Vasquez <vasquez.limon@gmail.com>
2024-01-08 15:53:13 -0800

    63

--- 60 to 69/chall63.py
+++ 60 to 69/chall63.py
@@ -1,13 +1,14 @@
 if __name__ == "__main__":
     N, M = map(int, input().split())
-    l1 = map(int, input().split())
-    
-    for i in range(1,M+1):
-        if i
-        l2= map(int, input().split())
+    L1 = list(map(int, input().split()))
+    A = set(map(int, input().split()))
+    B = set(map(int, input().split()))
 
-        
+    like = 0
+    for i in L1:
+        if i in A:
+            like += 1
+        if i in B:
+            like -= 1
 
-
-    # BC = int(input())
-    print(N, M)
+    print(like)
