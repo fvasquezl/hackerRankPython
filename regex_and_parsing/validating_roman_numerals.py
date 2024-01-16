581e228ecf4523ab3628220734a5c1f51a3a379f
Faustino Vasquez Limon <vasquez.limon@gmail.com>
2024-01-15 22:13:48 -0800

    validation roman number

---
+++ regex_and_parsing/validating_roman_numerals.py
@@ -0,0 +1,10 @@
+regex_pattern = r"^(?=[MDCLXVI])M{0,3}(?:CM|CD|D?C{0,3})(?:XC|XL|L?X{0,3})(?:IX|IV|V?I{0,3})$"  # Do not delete 'r'.
+
+import re
+
+print(str(bool(re.match(regex_pattern, input()))))
+
+
+"""
+https://developers.google.com/edu/python/regular-expressions
+"""
