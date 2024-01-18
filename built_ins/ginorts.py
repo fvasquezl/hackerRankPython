05300676e2520d9678827115fad8eed55aaa1ef2
Faustino Vasquez <vasquez.limon@gmail.com>
2024-01-18 15:03:12 -0800

    sorting

--- built_ins/ginorts.py
+++ built_ins/ginorts.py
@@ -1,10 +1,19 @@
 """Main Module"""
 
+import re
+
 
 def sorting(x):
     return x.lower()
 
 
 if __name__ == "__main__":
-    l1 = input()
-    print(sorted(l1))
+    r = []
+    s = input()
+
+    l = sorted(re.findall(r"[a-z]", s))
+    L = sorted(re.findall(r"[A-Z]", s))
+    n = sorted(re.findall(r"\d", s))
+    o = [x for x in n if int(x) % 2 != 0]
+    e = [x for x in n if int(x) % 2 == 0]
+    print("".join(l + L + o + e))
