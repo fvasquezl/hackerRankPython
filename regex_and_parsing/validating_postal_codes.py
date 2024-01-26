4e732849c38a6aa18a1ec7e9d44aaf333166fe9f
Faustino Vasquez Limon <vasquez.limon@gmail.com>
2024-01-25 23:32:11 -0800

    validation postal codes complete

--- regex_and_parsing/validating_postal_codes.py
+++ regex_and_parsing/validating_postal_codes.py
@@ -1,17 +1,12 @@
 regex_integer_in_range = r"^[1-9]\d{5}$"  # Do not delete 'r'.
-regex_alternating_repetitive_digit_pair = r"(\d)\d\1"  # Do not delete 'r'.
+regex_alternating_repetitive_digit_pair = r"(\d)(?=\d\1)"  # Do not delete 'r'.
 
 
 import re
 
 P = input()
 
-# print(
-#     bool(re.match(regex_integer_in_range, P))
-#     and len(re.findall(regex_alternating_repetitive_digit_pair, P)) < 2
-# )
-
-print(len(re.findall(regex_alternating_repetitive_digit_pair, P)) < 2)
-
-
-"""  Programa en progreso  """
+print(
+    bool(re.match(regex_integer_in_range, P))
+    and len(re.findall(regex_alternating_repetitive_digit_pair, P)) < 2
+)
